bfdeead5f377dfd10e954abc8e611c33ab7ff4c7
DS Rathore <dsrathorebsw1234@gmail.com>
2021-08-03 14:46:45 +0530

    Counter

---
+++ Data Structures/Useful Questions/Counter.py
@@ -0,0 +1,14 @@
+"""
+
+    #! Print the three most common characters along with their occurrence count.
+    #! Sort in descending order of occurrence count.
+    #! If the occurrence count is the same, sort the characters in alphabetical order.
+
+"""
+
+ar = [2, 3, 2, 2, 3, 5, 7, 4, 4, 4, 6]
+
+s = ar.count(2)
+print(s)
+ar.sort()
+print(ar)
